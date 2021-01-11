7cb98f15477990b54aa863f8a113f05b75eb1a98
jinzhan <jinzhan@baidu.com>
2021-01-11 17:15:08 +0800

    up

--- src/index.js
+++ src/index.js
@@ -11,6 +11,9 @@
 // import App from './components/js/comp-global-store/index';
 import App from './App';
 
+window.$app = App;
+
 const app = new App();
 app.attach(document.body);
+
 console.log('---- this is main index ---');
